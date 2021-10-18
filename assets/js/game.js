cbdfc2ec4f7d6b9d441551f3a5ce9ebf1238d28c
Claire <claire@Claires-MacBook-Pro-3.local>
2021-10-18 14:10:44 -0400

    fixed blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,16 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name ===null) {
+        name = prompt("What is your robot's name?");
+    }
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -111,6 +119,7 @@ var startGame = function() {
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+            debugger;
             var pickedEnemyObj = enemyInfo[i];
             //give enemy random Health, between 40 and 60
             pickedEnemyObj.health = randomNumber(40, 60);
